1f0553989ab5b98b563fec47a94667bc2d90ece0
MrNyce <nyceguysean@gmail.com>
2021-08-10 13:24:31 -0400

    Add the game logic--following the computer-selected hand.

--- script.js
+++ script.js
@@ -19,3 +19,39 @@ function computerPlay(){
 
     return hand;
 }
+
+function roShamBo(playerSelection, ComputerSelection=computerPlay()){
+    if (playerSelection.toLowerCase()=="rock"){
+        if (ComputerSelection=="paper"){
+            return "You lose! Paper beats Rock."
+        }
+        else if (ComputerSelection=="scissors"){
+            return "You win! Rock beats Scissors."
+        }
+        else if (ComputerSelection=="rock"){
+            return "It's a tie! Rock matches rock."
+        }
+    }
+    else if (playerSelection.toLowerCase()=="paper"){
+        if (ComputerSelection=="scissors"){
+            return "You lose! Scissors beats paper."
+        }
+        else if (ComputerSelection=="rock"){
+            return "You win! Paper beats rock."
+        }
+        else if (ComputerSelection=="paper"){
+            return "It's a tie! Paper matches paper."
+        }
+    }
+    else if (playerSelection.toLowerCase()=="scissors"){
+        if (ComputerSelection=="rock"){
+            return "You lose! Rock beats scissors."
+        }
+        else if (ComputerSelection=="paper"){
+            return "You win! Scissors beat paper."
+        }
+        else if (ComputerSelection=="scissors"){
+            return "It's a tie! Scissors match scissors."
+        }
+    }
+}
